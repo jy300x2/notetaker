38af87e3a4944b5ae9a9e7499f67094a22b0de52
Jon Yu <jy300x2@gmail.com>
2016-06-01 03:16:49 +0000

    Added controller and root page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Notetaker::Application.routes.draw do
+  root 'notes#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
