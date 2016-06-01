2ed95ed4437db5fd82734d14348402a5f49bfa40
Jon Yu <jy300x2@gmail.com>
2016-06-01 03:04:07 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Notetaker::Application.config.secret_key_base = 'be13d1315c8a445dfcb133f244deb70b5c8106a9c5491c54432320adbc446276905d19bac801d05b5f84cb90bf8a3a67d19c8f8b6071dc54ca406a2d971e1d49'
